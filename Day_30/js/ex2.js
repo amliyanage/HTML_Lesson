54e5f74f2985fce86b689de016eb7c46a1ab020e
amliyanage <ashenmliyanage@gmail.com>
2024-03-30 03:06:26 -0700

    variable of js

---
+++ Day_30/js/ex2.js
@@ -0,0 +1,15 @@
+
+let boolean = true;
+let number = 1;
+let String = "Hello World";
+let address;
+let mail = undefined;
+let contact = null;
+
+console.log(typeof boolean); 
+console.log(typeof String);
+console.log(typeof number);
+console.log(typeof address);
+console.log(typeof mail);
+console.log(typeof contact);
+
